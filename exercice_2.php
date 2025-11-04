246772eaec7de03d0ee2a1c710649aa90b8e4ad9
Vityy <ygensoftvd@gmail.com>
2025-11-04 12:32:56 +0100

    Exercice 2 Termine

---
+++ exercice_2.php
@@ -0,0 +1,29 @@
+<?php
+
+$anneesNaissance = [2010, 1994, 1957, 2017, 2003, 1998, 1930, 1918, 1945, 2023];
+
+$anneeMin = 2025;
+$anneeMax = 0;
+$agesPairs = 0;
+
+foreach($anneesNaissance as $anneeNaissance)
+{
+    if($anneeNaissance < $anneeMin)
+    {
+        $anneeMin = $anneeNaissance;
+    }
+
+    if($anneeNaissance > $anneeMax)
+    {
+        $anneeMax = $anneeNaissance;
+    }
+
+    if((2025 - $anneeNaissance) % 2 == 0)
+    {
+        $agesPairs++;
+    }
+}
+
+echo "L'année de naissance de la personne la plus agée est: " . $anneeMin . "\n";
+echo "L'année de naissance de la personne la plus jeune est: " . $anneeMax . "\n";
+echo "Le nombre de personnes dont l'âge est pair est: " . $agesPairs . "\n";
